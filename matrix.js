7c6debe000ba5f1efb1b5b4ee94e11a1cafcfed4
Ivan Vieira <iivnn00@gmail.com>
2020-10-28 11:17:32 -0300

    mobile

--- matrix.js
+++ matrix.js
@@ -1,31 +1,30 @@
-const varietyOfCharacters = 256;
-const canvas = document.getElementById("matrix_effect");
-const width = canvas.width;
-const height = canvas.height;
-const context = canvas.getContext("2d");
-const columnsCount = Math.floor(width / 20) * 2;
-const yPos = Array(columnsCount).fill(0);
+var varietyOfCharacters = 256;
+var canvas = document.getElementById("matrix_effect");
+var width = canvas.width;
+var height = canvas.height;
+var context = canvas.getContext("2d");
+var columnsCount = Math.floor(width / 20) * 2;
+var yPos = Array(columnsCount).fill(0);
 
-context.fillStyle = "rgb(0, 20, 0)"; // background color
-context.fillRect(0, 0, width, height); //draws the background
+context.fillStyle = "rgb(0, 10, 0)";
+context.fillRect(0, 0, width, height);
 
+var text, xPos;
 function matrix() {
-    context.fillStyle = "rgb(0, 20, 0, .1)";
+    context.fillStyle = "rgb(0, 10, 0, .1)";
     context.fillRect(0, 0, width, height);
     context.fillStyle = "rgb(0, 256, 0, .7)";
-    context.font = "20px Modeseven-L3n5";
-    yPos.map((y, index) => {
-        let text = String.fromCharCode(Math.floor(Math.random() * varietyOfCharacters));
-        let xPos = index * 10;
-        context.fillText(text, xPos, y);
-        if (y > Math.random() * height * 20) {
-            yPos[index] = 0;
-        } else {
-            yPos[index] = y + 20;
-        }
-    });
+    context.font = "22px Modeseven-L3n5";
+    yPos.map(function (y, index) {
+            text = String.fromCharCode(Math.floor(Math.random() * varietyOfCharacters));
+            xPos = index * 10;
+            context.fillText(text, xPos, y);
+            if (y > Math.random() * height * 20) {
+                yPos[index] = 0;
+            } else {
+                yPos[index] = y + 20;
+            }
+        });
 }
 
-if (window.screen.width > 720) {
-    window.setInterval(matrix, 75);
-} 
+window.setInterval(matrix, 75);
